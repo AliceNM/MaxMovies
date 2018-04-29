1b0a2aeead69d981a74a7775a7a7b2b207e7ef5a
Alicemaina <alicemainachege@gmail.com>
2018-04-29 12:16:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/alice/maxmovies/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.alice.maxmovies;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button mFindMoviesButton = findViewById(R.id.findMoviesButton);
+        mFindMoviesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
